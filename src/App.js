5b5faede335ca3c60a293a7baee17618210b44b1
khalid sadig <khalid.sadig@yahoo.com>
2021-10-01 17:23:27 +0200

    Add responsiveness

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import TodoApp from "./components/TodoApp"
 function App() {
   return (
       <div className={'app'}>
-        <img src="./images/bg-mobile-light.jpg" alt="header"/>
+        <img className ={'mobile-header'} src="./images/bg-mobile-light.jpg" alt="header"/>
+        <img className = {'desktop-header'} src="./images/bg-desktop-light.jpg" alt="header"/>
                 <TodoApp/>
         </div>
   );
