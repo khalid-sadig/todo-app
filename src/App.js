daa5e758be8ad6b68360deabd23cdd5cf47bec6e
khalid-sadig <khalid.sadig@yahoo.com>
2022-05-13 04:34:40 +0200

    make it responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import TodoApp from "./components/TodoApp"
+import TodoApp from "./components/TodoApp";
 
 function App() {
   return (
-      <div className={'app'}>
-        <img className ={'mobile-header'} src="./images/bg-mobile-light.jpg" alt="header"/>
-        <img className = {'desktop-header'} src="./images/bg-desktop-light.jpg" alt="header"/>
-                <TodoApp/>
-        </div>
+    <div className={"app"}>
+      <TodoApp />
+    </div>
   );
 }
 
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,60 +1,67 @@
-import {useState} from 'react'
-import AddTodo from "./AddTodo"
-import ListTodo from "./ListTodo"
+import { useState } from "react";
+import AddTodo from "./AddTodo";
+import ListTodo from "./ListTodo";
 
+function TodoApp() {
+  const [todoText, setTodoText] = useState("What needs to be done?");
+  const [todoList, setTodoList] = useState([]);
+  const [filter, setFilter] = useState("all");
 
-function TodoApp(){
-  const [todoText, setTodoText] = useState('What needs to be done?')
-  const [todoList, setTodoList] = useState([])
-  const [filter, setFilter] = useState('all')
-  
-
-  function handleTodoTextChange(newTodoText){
-    setTodoText(newTodoText)
+  function handleTodoTextChange(newTodoText) {
+    setTodoText(newTodoText);
   }
 
-  function addTodo(){
+  function addTodo() {
     setTodoList((prev) => {
-      return [...prev,{text: todoText, completed: false}]
-    })
+      return [...prev, { text: todoText, completed: false }];
+    });
 
-    setTodoText('')
+    setTodoText("");
   }
 
-  function handleTodoDeletion(index){
-    setTodoList(prev => {
-      let newTodoList  = [...prev]
-      newTodoList.splice(index, 1)
-      return [...newTodoList]
-    })
+  function handleTodoDeletion(index) {
+    setTodoList((prev) => {
+      let newTodoList = [...prev];
+      newTodoList.splice(index, 1);
+      return [...newTodoList];
+    });
   }
 
-  function handleTaskStatusChange(completed, index){
-    setTodoList(prev => {
-      let newTodoList = [...prev]
-      newTodoList[index].completed = !completed
-      return [...newTodoList]
-    })  
+  function handleTaskStatusChange(completed, index) {
+    setTodoList((prev) => {
+      let newTodoList = [...prev];
+      newTodoList[index].completed = !completed;
+      return [...newTodoList];
+    });
   }
 
-  function handleFilterClick(e){
-    setFilter(e.target.textContent)
+  function handleFilterClick(e) {
+    setFilter(e.target.textContent);
   }
-    return(
-      <div className = {'TodoApp'}>
-          <div className ={'flex-container'}>
-            <h2 className ={'app-header'}>todo</h2>
-            <img className = {'moon'} src={'../images/icon-moon.svg'} alt ="toggle mode"/>
-          </div>
-          <AddTodo todoText = {todoText} onTodoTextChange = {handleTodoTextChange} onAddTodo = {addTodo}/>
-          <ListTodo filter ={filter} todoList ={todoList} onTodoDeletion = {handleTodoDeletion} onTaskStatusChange = {handleTaskStatusChange}/>
-          <div className = {'filter'}>
-                <button onClick ={handleFilterClick}>all</button>
-                <button onClick = {handleFilterClick}>active</button>
-                <button onClick = {handleFilterClick}>completed</button>
-            </div>
+  return (
+    <div className="TodoApp">
+      <div className="background">
+        <h2 className={"app-header"}>todo</h2>
+
+        <AddTodo
+          todoText={todoText}
+          onTodoTextChange={handleTodoTextChange}
+          onAddTodo={addTodo}
+        />
       </div>
-    )
+      <ListTodo
+        filter={filter}
+        todoList={todoList}
+        onTodoDeletion={handleTodoDeletion}
+        onTaskStatusChange={handleTaskStatusChange}
+      />
+      <div className={"filter"}>
+        <button onClick={handleFilterClick}>all</button>
+        <button onClick={handleFilterClick}>active</button>
+        <button onClick={handleFilterClick}>completed</button>
+      </div>
+    </div>
+  );
 }
 
-export default TodoApp
+export default TodoApp;
